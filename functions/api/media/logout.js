687608702394cdec3e7edbe2c40ca43f3dd3192b
Geordie Mac <Joshua.McManus2006@gmail.com>
2026-07-30 17:04:10 +0100

    Modify logout function to accept request parameter
    Pass the request object to clearCookieHeader for logout.

--- functions/api/media/logout.js
+++ functions/api/media/logout.js
@@ -1,5 +1,6 @@
 import { clearCookieHeader, json } from '../../_utils.js';
 
-export async function onRequestPost() {
-  return json({ ok: true }, { headers: { 'Set-Cookie': clearCookieHeader('media_auth') } });
+export async function onRequestPost({ request }) {
+  // ✅ PASS `request` TO clearCookieHeader HERE:
+  return json({ ok: true }, { headers: { 'Set-Cookie': clearCookieHeader('media_auth', request) } });
 }
