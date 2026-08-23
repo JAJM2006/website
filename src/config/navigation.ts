6185eaccef72b4ba4a49e8f88d99c1e5dec3bb48
Geordie Mac <Joshua.McManus2006@gmail.com>
2026-08-24 00:52:13 +0100

    Fix to v1.5 - 2) Forgot to set to true, again...

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -3,7 +3,7 @@ import type { NavLink } from "../types";
 export const NAV_LINKS: NavLink[] = [
     { href: "/", label: "About", isActive: true },
     { href: "/publications", label: "Publications", isActive: true },
-    { href: "/suggestions", label: "Suggestions", isActive: false },
+    { href: "/suggestions", label: "Suggestions", isActive: true },
     { href: "/teaching", label: "Teaching", isActive: true },
     { href: "/projects", label: "Code", isActive: false },
     { href: "/posts", label: "Blog", isActive: false },
